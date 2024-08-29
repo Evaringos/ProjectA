b1d646a4f702662b258349f90c2e0c5228cb2f69
= <evc1evcus@gmail.com>
2024-08-29 14:53:10 +0300

    vscode folder deleted / testing output script added

---
+++ Assets/Console speaker.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consolespeaker : MonoBehaviour
+{
+    bool counter = false;
+    int number = 0;
+    
+    void Start()
+    {
+        print("Starting of counter:");
+        StartCoroutine(CountWithDelay());
+    }
+
+    IEnumerator CountWithDelay()
+    {
+        while (!counter)
+        {
+            yield return new WaitForSeconds(1); // Пауза в одну секунду
+            print(number + 1);
+            number++;
+
+            if (number >= 10)
+            {
+                counter = true;
+                Debug.Log("Counting is ended - this is test log");
+                Debug.LogWarning("Counting is ended - this is test log warning");
+                Debug.LogError("Counting is ended - this is test log error");
+            }
+        }
+    }
+}
